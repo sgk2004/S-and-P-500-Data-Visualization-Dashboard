e1b095669a3e721960c90462471ea2c184cd410b
jran14 <jennierann@gmail.com>
2020-02-14 17:51:41 -0600

    fetch/app

--- fetch.py
+++ fetch.py
@@ -1,134 +1,158 @@
 from flask import Flask, render_template, jsonify
-from app import fetch_stock2018, fetch_sectors2018
+from app import all_stock, fetch_stock2018, fetch_financial_indicator_2018, fetch_financial_indicator_2017, fetch_financial_indicator_2016, fetch_financial_indicator_2015, fetch_financial_indicator_2014,fetch_stock
 import pandas as pd
 import datetime as dt
 
 # Flask app
 
 app = Flask(__name__)
+# ********HOME PAGE*******************
 
-#HOME PAGE- AMZN
+#SECTOR HOMEPAGE FOR 2018
+@app.route('/home')
+def sectorHome():
+    stocks= fetch_stock2018('Technology')
+    #read to dataframe
+    stocks_df2018= pd.DataFrame(stocks)
+    print(stocks_df2018)
+    response= jsonify(stocks_df2018.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    print("***************")
+    print(response.headers)
+    return response
+
+#HOME PAGE- APPLE
 @app.route('/')
 def home():
-    return render_template('chart.html')
-    
-# #TICKER
-# @app.route('/<selected_ticker>')
-# def hello(selected_ticker):
-#     stocks= all_stock()
-#     #read to dataframe
+    selected_ticker= 'AAPL' 
+    stocks= all_stock(selected_ticker)
+    #read to dataframe
+    stocks_df= pd.DataFrame(stocks)
+    print(stocks_df)
+    response= jsonify(stocks_df.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+# *******************SECTOR*************************************
+
+#SECTOR SELECTION FOR 2018
+@app.route('/sector/<selected_sector>/')
+def sector(selected_sector):
+    stocks= fetch_stock2018(selected_sector)
+    #read to dataframe
+    stocks_df2018= pd.DataFrame(stocks)
+    stocks_df2018= stocks_df2018.nlargest(10,'Revenue')
+    print(stocks_df2018)
+    response= jsonify(stocks_df2018.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+    # -----------------------------------------------------
+   
+
+# *********************FINANCIAL INDICATOR BY TICKER************************
+#COMPANY SELECTION FOR 2014-18
+@app.route('/2018/<selected_ticker>/')
+def company_ticker2018(selected_ticker):
+    stocks= fetch_financial_indicator_2018(selected_ticker)
+    #read to dataframe
+    stocks_df2018= pd.DataFrame(stocks)
+    print(stocks_df2018)
+    response= jsonify(stocks_df2018.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/2017/<selected_ticker>/')
+def company_ticker2017(selected_ticker):
+    stocks= fetch_financial_indicator_2017(selected_ticker)
+    #read to dataframe
+    stocks_df2018= pd.DataFrame(stocks)
+    print(stocks_df2018)
+    response= jsonify(stocks_df2018.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/2016/<selected_ticker>/')
+def company_ticker2016(selected_ticker):
+    stocks= fetch_financial_indicator_2016(selected_ticker)
+    #read to dataframe
+    stocks_df2018= pd.DataFrame(stocks)
+    print(stocks_df2018)
+    response= jsonify(stocks_df2018.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/2015/<selected_ticker>/')
+def company_ticker2015(selected_ticker):
+    stocks= fetch_financial_indicator_2015(selected_ticker)
+    #read to dataframe
+    stocks_df2018= pd.DataFrame(stocks)
+    print(stocks_df2018)
+    response= jsonify(stocks_df2018.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/2014/<selected_ticker>/')
+def company_ticker2014(selected_ticker):
+    stocks= fetch_financial_indicator_2014(selected_ticker)
+    #read to dataframe
+    stocks_df2018= pd.DataFrame(stocks)
+    print(stocks_df2018)
+    response= jsonify(stocks_df2018.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@app.route('/sector/')
+def sectorfordropdown():
+    stocks= fetch_stock()
+    #read to dataframe
+    stocks_df2018= pd.DataFrame(stocks)
+    print(stocks_df2018)
+    response= jsonify(stocks_df2018.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+# ******************TIME SERIES GRAPH BY TICKER******************************
+#TICKER
+@app.route('/<selected_ticker>')
+def hello(selected_ticker):
+    stocks= all_stock(selected_ticker)
+    #read to dataframe
+    stocks_df= pd.DataFrame(stocks)
+    print(stocks_df)
+    response= jsonify(stocks_df.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+#FREQUENCY
+@app.route('/<selected_ticker>/<freq>')
+def frequency(selected_ticker, freq):
+    stocks= all_stock(selected_ticker)
+    stocks_df= pd.DataFrame(stocks)
+    # stocks_df= stocks_df.loc[stocks_df['ticker'] == selected_ticker]    
+    stocks_df['date']=pd.to_datetime(stocks_df['date'], format='%Y-%m-%d', errors='coerce')
+    stocks_df['month']= pd.DatetimeIndex(stocks_df['date']).month
+    stocks_df= stocks_df.resample(freq, on='date').mean()
+    print(stocks_df)
+    response= jsonify(stocks_df.to_dict('records'))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+    # *************************************************************
+#     @app.route('/<selected_ticker>/<freq>')
+# def frequency(selected_ticker, freq):
+#     stocks= all_stock(selected_ticker)
 #     stocks_df= pd.DataFrame(stocks)
-#     stocks_df= stocks_df.loc[stocks_df['ticker'] == selected_ticker]
+    
+#     stocks_df['date']=pd.to_datetime(stocks_df['date'], format='%Y-%m-%d', errors='coerce')
+#     stocks_df['month']= pd.DatetimeIndex(stocks_df['date']).month
+#     stocks_df= stocks_df.resample(freq, on='date').mean()
 #     print(stocks_df)
 #     response= jsonify(stocks_df.to_dict('records'))
 #     response.headers.add('Access-Control-Allow-Origin', '*')
 #     return response
-
-# #FREQUENCY
-# @app.route('/<selected_ticker>/<freq>')
-# def frequency(selected_ticker, freq):
-#     stocks= all_stock()
-#     stocks_df= pd.DataFrame(stocks)
-#     stocks_df= stocks_df.loc[stocks_df['ticker'] == selected_ticker]
-#     stocks_df['date']=pd.to_datetime(stocks_df['date'], format='%Y-%m-%d', errors='coerce')
-#     stocks_df['month']= pd.DatetimeIndex(stocks_df['date']).month
-#     stocks_df= stocks_df.resample(freq, on='date').mean()
-#     # print(stocks_df)
-#     response= jsonify(stocks_df.to_dict('records'))
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
-# #COMPANY SELECTION FOR 2018
-# @app.route('/company/<selected_ticker>/')
-# def company_ticker(selected_ticker):
-#     stocks= fetch_stock2018()
-#     #read to dataframe
-#     stocks_df2018= pd.DataFrame(stocks)
-#     print(stocks_df2018)
-#     stocks_df2018= stocks_df2018.loc[stocks_df2018['ticker'] == selected_ticker]
-#     # print(stocks_df2018)
-#     response= jsonify(stocks_df2018.to_dict('records'))
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
-@app.route('/company')
-def companyHome():
-    stocks= fetch_stock2018()
-    #read to dataframe
-    stocks_df2018= pd.DataFrame(stocks)
-    # print(stocks_df2018)
-    response= jsonify(stocks_df2018.to_dict('records'))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-#SECTOR SELECTION FOR 2018
-# @app.route('/sector/<selected_sector>/')
-# def sector(selected_sector):
-#     stocks= fetch_stock2018()
-#     #read to dataframe
-#     stocks_df2018= pd.DataFrame(stocks)
-#     # print(stocks_df2018)
-#     stocks_df2018= stocks_df2018.loc[stocks_df2018['Sector'] == selected_sector]
-#     print(stocks_df2018)
-#     response= jsonify(stocks_df2018.to_dict('records'))
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
-#SECTOR HOMEPAGE FOR 2018
-@app.route('/topten/sector/<topselect>/')
-def sectorTop(topselect):
-    stocks= fetch_stock2018()
-    #read to dataframe
-    stocks_df2018= pd.DataFrame(stocks)
-    stocks_df2018= stocks_df2018.loc[stocks_df2018['Sector'] == topselect]
-    stocks_df2018['Revenue'] = pd.to_numeric(stocks_df2018['Revenue'])
-    stocks_df2018= stocks_df2018.nlargest(10,'Revenue')
-    print('TESTESTEST')
-    print(stocks_df2018)
-    response= jsonify(stocks_df2018.to_dict('records'))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    print(response.headers)
-    return response
-
-@app.route('/sector/<selected_sector>/')
-def sector(selected_sector):
-    stocks= fetch_sectors2018(selected_sector)
-    #read to dataframe
-    stocks_df2018= pd.DataFrame(stocks)
-    stocks_df2018= stocks_df2018.nlargest(10,'Revenue')
-    # print(stocks_df2018)
-    # stocks_df2018= stocks_df2018.loc[stocks_df2018['Sector'] == selected_sector]
-    print(stocks_df2018)
-    response= jsonify(stocks_df2018.to_dict('records'))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-# @app.route('/sectors/')
-# def sectoronly():
-#     stocks= fetch_unique_sectors()
-#     #read to dataframe
-#     # stocks_df2018= pd.DataFrame(stocks)
-#     # stocks_df2018= stocks_df2018.unique('Sector')
-#     # print(stocks_df2018)
-#     # stocks_df2018= stocks_df2018.loc[stocks_df2018['Sector'] == selected_sector]
-#     print(stocks)
-#     response= jsonify(stocks)
-#     response.headers.add('Access-Control-Allow-Origin', '*')
-#     return response
-
-@app.route('/EPS/<selected_sector>/')
-def sectorEPS(selected_sector):
-    stocks= fetch_sectors2018(selected_sector)
-    #read to dataframe
-    stocks_df2018= pd.DataFrame(stocks)
-    stocks_df2018= stocks_df2018.nlargest(10,'EPS')
-    # print(stocks_df2018)
-    # stocks_df2018= stocks_df2018.loc[stocks_df2018['Sector'] == selected_sector]
-    print(stocks_df2018)
-    response= jsonify(stocks_df2018.to_dict('records'))
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
+    
+# **************************************************************************
 
 if __name__ == '__main__':
     app.run(debug=True )
